01009203ef3f73bac9573726bdf2204045e991f3
Gwin14 <fabiospinheira@gmail.com>
2024-06-28 19:49:09 -0300

    profile image

--- notesApp/forms.py
+++ notesApp/forms.py
@@ -1,8 +1,8 @@
-from . import models
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.core.exceptions import ValidationError
 from django.contrib.auth.models import User
+from . import models
 
 
 class NoteForm(forms.ModelForm):
@@ -20,7 +20,7 @@ class NoteForm(forms.ModelForm):
 
 class RegisterForm(UserCreationForm):
     first_name = forms.CharField(required=True)
-    email = forms.CharField(required=True)
+    email = forms.EmailField(required=True)
     image = forms.ImageField(required=False)
 
     class Meta:
@@ -30,7 +30,19 @@ class RegisterForm(UserCreationForm):
 
     def clean_email(self):
         email = self.cleaned_data.get('email')
-
         if User.objects.filter(email=email).exists():
-            self.add_error('email', ValidationError(
-                'Já existe esse email', code='invalid'))
+            raise ValidationError('Este email já está em uso.')
+        return email
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.first_name = self.cleaned_data['first_name']
+        user.email = self.cleaned_data['email']
+
+        if commit:
+            user.save()
+            profile, created = models.Profile.objects.get_or_create(user=user)
+            if 'image' in self.cleaned_data:
+                profile.image = self.cleaned_data['image']
+                profile.save()
+        return user
--- notesApp/models.py
+++ notesApp/models.py
@@ -7,8 +7,8 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Note(models.Model):
-    title = models.CharField(max_length=40,default='Título')
-    content = models.TextField(blank=True, null=True,default='Conteúdo')
+    title = models.CharField(max_length=40, default='Título')
+    content = models.TextField(blank=True, null=True, default='Conteúdo')
     date_created = models.DateTimeField(default=timezone.now)
     tags = TaggableManager()
     picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m/')
@@ -17,3 +17,12 @@ class Note(models.Model):
 
     def __str__(self) -> str:
         return f'{self.title}'
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(
+        upload_to='profile_images', blank=True, null=True)
+
+    def __str__(self):
+        return self.user.username
